e144368d92f5367bfa14b88de53690eeeb6dc6ea
Laarni Bernardo <laarnijb@gmail.com>
2021-02-23 11:23:47 -0500

    Moved function createButton before the for loop.

--- exercises/exer58/script.js
+++ exercises/exer58/script.js
@@ -2,17 +2,16 @@
 const div = document.querySelector('#container');
 const colorArr = ['turquoise', 'peach', 'spearmint', 'rosewater', 'yellow'];
 
-for (let i = 1, j = 1; i <= 100;) {
-    for (let color of colorArr) {
-        div.appendChild(createButton(i, color));
-        i++;
-    }
-}
-
-
 function createButton (text, color) {
   let newButton = document.createElement('button');
   newButton.innerText = `Button #${text}`;
   newButton.classList.add(color);
   return newButton;
+}
+
+for (let i = 1, j = 1; i <= 100;) {
+    for (let color of colorArr) {
+        div.appendChild(createButton(i, color));
+        i++;
+    }
 }
